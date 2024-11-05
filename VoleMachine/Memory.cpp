35ebc883072754afefba28a1b61c397df8b5a0fe
Someoneyeeting <57325386+Someoneyeeting@users.noreply.github.com>
2024-11-05 02:15:04 +0200

    final changes this time

--- VoleMachine/Memory.cpp
+++ VoleMachine/Memory.cpp
@@ -5,8 +5,6 @@ Memory::Memory() {
 	for (unsigned char i = 0; i < 254; i++) {
 		this->setCell(i, "00");
 	}
-	this->setCell(254, "c0");
-	this->setCell(255, "00");
 }
 
 string Memory::getCell(unsigned char cell) {
@@ -18,6 +16,6 @@ void Memory::setCell(unsigned char cell, string val) {
 	val[0] = tolower(val[0]);
 	val[1] = tolower(val[1]);
 	this->memory[cell] = val;
-	if (cell == 256 - 2)
+	if (cell >= 256 - 2)
 		this->memory[cell] = "c0";
 }
